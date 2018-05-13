9377afe12f93962ec0b58215e9129c9bdc69c6cd
Ngocruby <39007362+Ngocruby@users.noreply.github.com>
2018-05-13 11:35:56 +0200

    Kommentar

--- js/app.js
+++ js/app.js
@@ -1,88 +1,60 @@
-var format = d3.format(",");
+var width = window.outerWidth, // xac dinh chieu dai cua Browser -Detect Browser-Width
+    height = window.outerHeight; //xac dinh chieu cao
 
-// Set tooltips
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) {
-        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) + "</span>";
-    })
+var svg = d3.select("body") // chon body element
+    .append("svg") // them svg vao body - append la funktion
+    .attr("width", width) // set do dai cho svg - attr la atrribut
+    .attr("height", height)// set chieu cao cho svg
+    .append('g') // them element g vao trong svg - g viet tat cua group
+    .attr('class', 'map'); // them attribu class ten map - add attribut
 
-var width = window.outerWidth,
-    height = window.outerHeight;
+var projection = d3.geoMercator() // tao projection Mercator
+    .scale(200) // xet do zoom- so cang nho zoom cang nhieu
+    .translate([width / 2, height / 1.5]); // xac dinh toa do
 
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append('g')
-    .attr('class', 'map');
+var path = d3.geoPath().projection(projection); // xac dinh duong ve theo projection
 
-var projection = d3.geoMercator()
-    .scale(200)
-    .translate([width / 2, height / 1.5]);
-
-var path = d3.geoPath().projection(projection);
-
-svg.call(tip);
-
-queue()
-    .defer(d3.json, "json/world_countries.json")
+queue() // bat dau xep hang
+    .defer(d3.json, "json/world_countries.json") // dan file json
     .defer(d3.json, "json/youtube.json")
-    .await(ready);
+    .await(ready); // goi funktion ve ban do
 
-function ready(error, data, youtube) {
+function ready(error, world_countries, youtube) {
+    console.log(world_countries); // log la in ra trong console
     console.log(youtube);
-
-    var videos = youtube.videos;
+    var videos = youtube.videos; // videos va overlay la objekt trong file youtube goi ra de su dung
     var overlay = youtube.overlay;
 
     svg.append("g")
-        .attr("class", "countries")
-        .selectAll("path")
-        .data(data.features)
-        .enter().append("path")
-        .attr("d", path)
+        .attr("class", "countries") 
+        .selectAll("path") // chon tat ca path trong group countries
+        .data(world_countries.features) //features la objekt trong world_countries
+        .enter().append("path") // hanh dong ve
+        .attr("d", path) // them atrribut net ve la d
         .style("fill", function(country) {
-            console.log(country);
             var id = country.id; // Get Country ID in World Map
-            var video = videos.filter(function(video) {
+            var filterVideos = videos.filter(function(video) {
                 return video.alpha3Code === id; // check if country ID in Videos is the same as country ID in World Mao
-            });
-            console.log(video); // Print video
-            if (video[0]) { // Check if video exists
-                var videoId = video[0].video.id; // Get Video ID
+            }); // return la truth or falsch tuong duong vs if
+            console.log(filterVideos);
+            if (filterVideos[0]) { // Check if video exists
+                var videoId = filterVideos[0].video.id; // Get Video ID
                 var color = overlay[videoId].color; // Get Color
                 return color;
             }
             return '#aaaaaa';
         })
-        .style('stroke', 'white')
-        .style('stroke-width', 1.5)
-        .style("opacity", 0.8)
-        // tooltips
-        .style("stroke", "white")
-        .style('stroke-width', 0.3)
-        .on('mouseover', function(d) {
-            tip.show(d);
-
-            d3.select(this)
+        .style('stroke', 'white') // cho vien mau trang
+        .style('stroke-width', 1.5) // do dam nhat cua vien
+        .style("opacity", 0.8) // do transparent
+        .on('mouseover', function(d) { // neu de chuot vao
+            d3.select(this) // this dc tu hieu la path vi selectALL o tren
                 .style("opacity", 1)
-                .style("stroke", "white")
                 .style("stroke-width", 3);
         })
-        .on('mouseout', function(d) {
-            tip.hide(d);
-
+        .on('mouseout', function(d) { // neu di chuot ra
             d3.select(this)
                 .style("opacity", 0.8)
-                .style("stroke", "white")
-                .style("stroke-width", 0.3);
-        });
-
-    svg.append("path")
-        .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-        // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
-        .attr("class", "names")
-        .attr("d", path);
+                .style("stroke-width", 1.5);
+        });    
 }
