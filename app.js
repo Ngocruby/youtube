16af94c060d2e94ebcc950c955ab59f3861f0446
Ngocruby <39007362+Ngocruby@users.noreply.github.com>
2018-05-13 10:06:06 +0200

    Format Code

--- app.js
+++ app.js
@@ -2,30 +2,30 @@ var format = d3.format(",");
 
 // Set tooltips
 var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) +"</span>";
-            })
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) + "</span>";
+    })
 
-var margin = {top: 0, right: 0, bottom: 0, left: 0},
-            width = window.outerWidth - margin.left - margin.right,
-            height = window.outerHeight - margin.top - margin.bottom;
+var margin = { top: 0, right: 0, bottom: 0, left: 0 },
+    width = window.outerWidth - margin.left - margin.right,
+    height = window.outerHeight - margin.top - margin.bottom;
 
 var color = d3.scaleThreshold()
-    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
+    .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
+    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
 
 var svg = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append('g')
-            .attr('class', 'map');
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append('g')
+    .attr('class', 'map');
 
 var projection = d3.geoMercator()
-                   .scale(200)
-                  .translate([width/2, height/1.5]);
+    .scale(200)
+    .translate([width / 2, height / 1.5]);
 
 var path = d3.geoPath().projection(projection);
 
@@ -37,57 +37,57 @@ queue()
     .await(ready);
 
 function ready(error, data, youtube) {
-  console.log(youtube);
+    console.log(youtube);
 
-  var videos = youtube.videos;
-  var overlay = youtube.overlay;
+    var videos = youtube.videos;
+    var overlay = youtube.overlay;
 
-  svg.append("g")
-      .attr("class", "countries")
-    .selectAll("path")
-      .data(data.features)
-    .enter().append("path")
-      .attr("d", path)
-      .style("fill", function(country) {
-        console.log(country);
-        var id = country.id; // Get Country ID in World Map
-        var video = videos.filter(function(video) {
-          return video.alpha3Code === id; // check if country ID in Videos is the same as country ID in World Mao
-        });
-        console.log(video); // Print video
-        if (video[0]) { // Check if video exists
-          var videoId = video[0].video.id; // Get Video ID
-          var color = overlay[videoId].color; // Get Color
-          return color;
-        }
-        return '#aaaaaa';
-      })
-      .style('stroke', 'white')
-      .style('stroke-width', 1.5)
-      .style("opacity",0.8)
-      // tooltips
-        .style("stroke","white")
-        .style('stroke-width', 0.3)
-        .on('mouseover',function(d){
-          tip.show(d);
-
-          d3.select(this)
-            .style("opacity", 1)
-            .style("stroke","white")
-            .style("stroke-width",3);
+    svg.append("g")
+        .attr("class", "countries")
+        .selectAll("path")
+        .data(data.features)
+        .enter().append("path")
+        .attr("d", path)
+        .style("fill", function(country) {
+            console.log(country);
+            var id = country.id; // Get Country ID in World Map
+            var video = videos.filter(function(video) {
+                return video.alpha3Code === id; // check if country ID in Videos is the same as country ID in World Mao
+            });
+            console.log(video); // Print video
+            if (video[0]) { // Check if video exists
+                var videoId = video[0].video.id; // Get Video ID
+                var color = overlay[videoId].color; // Get Color
+                return color;
+            }
+            return '#aaaaaa';
         })
-        .on('mouseout', function(d){
-          tip.hide(d);
+        .style('stroke', 'white')
+        .style('stroke-width', 1.5)
+        .style("opacity", 0.8)
+        // tooltips
+        .style("stroke", "white")
+        .style('stroke-width', 0.3)
+        .on('mouseover', function(d) {
+            tip.show(d);
 
-          d3.select(this)
-            .style("opacity", 0.8)
-            .style("stroke","white")
-            .style("stroke-width",0.3);
+            d3.select(this)
+                .style("opacity", 1)
+                .style("stroke", "white")
+                .style("stroke-width", 3);
+        })
+        .on('mouseout', function(d) {
+            tip.hide(d);
+
+            d3.select(this)
+                .style("opacity", 0.8)
+                .style("stroke", "white")
+                .style("stroke-width", 0.3);
         });
 
-  svg.append("path")
-      .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-       // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
-      .attr("class", "names")
-      .attr("d", path);
+    svg.append("path")
+        .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
+        // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+        .attr("class", "names")
+        .attr("d", path);
 }
